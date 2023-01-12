b2e636fe2dc43789fe2eb5c54a4bc143d6b27f85
thititongumpun <thiti180536@gmail.com>
2023-01-12 19:45:09 +0700

    move sanity-cms

--- sanity-cms/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: '1jz3iy2r',
-    dataset: 'production'
-  }
-})
--- sanity-cms/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-import experience from './experience'
-import pageInfo from './pageInfo'
-import project from './project'
-import skill from './skill'
-import skillTypes from './skillTypes'
-import social from './social'
-
-export const schemaTypes = [pageInfo, social, skillTypes, skill, experience, project]
