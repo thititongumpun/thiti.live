0a56c079110b3e9d09e5d4161056bed35d55ec3c
thititongumpun <thiti180536@gmail.com>
2023-01-15 14:14:35 +0700

    add form responsive

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -17,12 +17,12 @@ export default function ContactMe({}: Props) {
     window.location.href = `mailto:thiti180536@gmail.com?subject=${formData.subject}&body=Hi, My name is ${formData.name}. ${formData.message} (${formData.email})`;
   };
   return (
-    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-x-7xl pt-10 justify-evenly mx-auto items-center">
+    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row  pt-10 justify-evenly mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-widest text-gray-500 text-2xl">
         ContactMe
       </h3>
 
-      <div className="flex flex-col space-y-10">
+      <div className="flex flex-col space-y-10 text-gray-400">
         <div className="space-y-10">
           <div className="flex items-center space-x-5 justify-center">
             <EnvelopeIcon className="h-7 w-7 animate-pulse" />
@@ -34,45 +34,55 @@ export default function ContactMe({}: Props) {
             <p className="text-2xl">Bangkok TH</p>
           </div>
         </div>
+        
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="grid grid-cols-2 gap-4 max-w-xl m-auto">
+            <div className="col-span-2 lg:col-span-1">
+              <input
+                type="text"
+                className="md:text-xl w-full contactInput"
+                placeholder="Name"
+                {...register("name")}
+              />
+            </div>
 
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col space-y-2 w-fit mx-auto"
-        >
-          <div className="flex space-x-2">
-            <input
-              {...register("name")}
-              placeholder="Name"
-              type="text"
-              className="contactInput"
-            />
-            <input
-              {...register("email")}
-              placeholder="Email"
-              type="text"
-              className="contactInput"
-            />
+            <div className="col-span-2 lg:col-span-1">
+              <input
+                type="text"
+                className=" md:text-xl w-full contactInput"
+                placeholder="Email Address"
+                {...register("email")}
+              />
+            </div>
+
+            <div className="col-span-2 lg:col-span-2">
+              <input
+                type="text"
+                className=" md:text-xl w-full contactInput"
+                placeholder="Subject"
+                {...register("subject")}
+              />
+            </div>
+
+            <div className="col-span-2">
+              <textarea
+                cols={30}
+                rows={8}
+                placeholder="Message"
+                className="md:text-xl w-full contactInput"
+                {...register("message")}
+              />
+            </div>
+
+            <div className="col-span-2 text-center">
+              <button
+                className="bg-gray-50 py-5 px-10 rounded-md text-black font-bold text-lg w-full"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
           </div>
-
-          <input
-            {...register("subject")}
-            placeholder="Subject"
-            type="text"
-            className="contactInput"
-          />
-
-          <textarea
-            {...register("message")}
-            placeholder="Message"
-            className="contactInput"
-          />
-
-          <button
-            className="bg-gray-50 py-5 px-10 rounded-md text-black font-bold text-lg"
-            type="submit"
-          >
-            submit
-          </button>
         </form>
       </div>
     </div>
